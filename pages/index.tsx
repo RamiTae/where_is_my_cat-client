004afa0fa93b303527d512e3d1ce022629b9aa9d
Sooji Kim <yh0gogo@gmail.com>
2020-12-20 18:27:11 +0900

    Build Graphql sample page

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { useQuery } from "@apollo/client";
+import QUERY_COUNTRIES from "./queryCountries.graphql";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  console.log('확인', QUERY_COUNTRIES);
+  const { data, loading, error } = useQuery(QUERY_COUNTRIES);
+  if (loading) {
+    return <p>Loading</p>;
+  }
+  if (error) {
+    return <p>Error</p>;
+  }
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Countries GraphQL</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Countries</h1>
+      <div>
+        {data.countries.map((country) => (
+          <div key={country._id}>{country.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
